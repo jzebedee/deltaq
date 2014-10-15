41c395725a4270a99c1ccb7c93eb2aafa33d349c
scorpion <scorpion.jze@gmail.com>
2014-10-14 22:48:16 -0500

    Added bspatch

---
+++ deltaq/BsPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deltaq
+{
+    public static class BsPatch
+    {
+        public static void Apply()
+        {
+            
+        }
+    }
+}
