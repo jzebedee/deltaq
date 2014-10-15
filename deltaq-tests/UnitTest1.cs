d486d62ff22488f2c29ba9dff7fba70eb38ce3a5
scorpion <scorpion.jze@gmail.com>
2014-10-14 22:39:39 -0500

    Initial commit

---
+++ deltaq-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace deltaq_tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
