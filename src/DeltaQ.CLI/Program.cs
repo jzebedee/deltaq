4894db96daed070c54f2ab657c375cecfb84054e
J. Zebedee <zebedee@code.gripe>
2021-02-03 14:12:32 -0500

    Add CLI stub

---
+++ src/DeltaQ.CLI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DeltaQ.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f1 = args[1];
+            var f2 = args[2];
+            var o = args[3];
+            switch(args[0])
+            {
+                case "diff":
+                    BsDiff.BsDiff.Create(File.ReadAllBytes(f1), File.ReadAllBytes(f2), File.OpenWrite(o));
+                    break;
+                case "patch":
+                    BsDiff.BsPatch.Apply(File.ReadAllBytes(f1), File.ReadAllBytes(f2), File.OpenWrite(o));
+                    break;
+            }
+        }
+    }
+}
