bb741556754003554d05fcfd03429b4a6eb1a74c
J. Zebedee <zebedee@code.gripe>
2021-05-09 09:19:57 -0500

    Add LibDivSufSort stub

---
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/LibDivSufSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Buffers;
+
+namespace DeltaQ.SuffixSorting.LibDivSufSort
+{
+    public class LibDivSufSort : ISuffixSort
+    {
+        public IMemoryOwner<int> Sort(ReadOnlySpan<byte> textBuffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sort(ReadOnlySpan<byte> textBuffer, Span<int> suffixBuffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
