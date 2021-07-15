b339b7dd17a472f5904d1a82ce6c3aff3f6275e7
J. Zebedee <zebedee@code.gripe>
2021-07-15 03:11:43 -0500

    interim commit

--- src/DeltaQ.SuffixSorting.LibDivSufSort/RsDivSufSort.cs
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/RsDivSufSort.cs
@@ -20,18 +20,18 @@ namespace DeltaQ.SuffixSorting.LibDivSufSort
 
             var n = T.Length;
 
-            switch(n)
+            switch (n)
             {
                 case 0: return;
                 case 1:
                     SA[0] = 0;
                     return;
-                //case 2:
-                //    if(T[0] < T[1])
-                //    {
-                //        SA.copy
-                //    }
-                //    break;
+                    //case 2:
+                    //    if(T[0] < T[1])
+                    //    {
+                    //        SA.copy
+                    //    }
+                    //    break;
             }
 
             var result = sort_typeBstar(T, SA);
@@ -149,239 +149,246 @@ namespace DeltaQ.SuffixSorting.LibDivSufSort
             }
             m = n - m;
 
-            //    // Note: A type B* suffix is lexicographically smaller than a type B suffix
-            //    // that beings with the same first two characters.
+            // Note: A type B* suffix is lexicographically smaller than a type B suffix
+            // that beings with the same first two characters.
 
-            //    // Calculate the index of start/end point of each bucket.
-            //    {
-            //        i = 0;
-            //        j = 0;
-            //        for c0 in 0..(ALPHABET_SIZE as Idx) {
-            //            // body
-            //            t = i + A[c0];
-            //            A[c0] = i + j; // start point
-            //            i = t + B.b()[(c0, c0)];
+            // Calculate the index of start/end point of each bucket.
+            {
+                i = 0;
+                j = 0;
+                for (c0 = 0; c0 < ALPHABET_SIZE; c0++)
+                {
+                    // body
+                    t = i + A[c0];
+                    A[c0] = i + j; // start point
+                    i = t + Bb[(c0, c0)];
 
-            //            for c1 in (c0 + 1)..(ALPHABET_SIZE as Idx) {
-            //                j += B.bstar()[(c0, c1)];
-            //                B.bstar()[(c0, c1)] = j; // end point
-            //                i += B.b()[(c0, c1)];
-            //            }
-            //        }
-            //    }
+                    for (c1 = c0 + 1; c1 < ALPHABET_SIZE; c1++)
+                    {
+                        j += Bstar[(c0, c1)];
+                        Bstar[(c0, c1)] = j; // end point
+                        i += Bb[(c0, c1)];
+                    }
+                }
+            }
 
-            //    if (0 < m) {
-            //        // Sort the type B* suffixes by their first two characters
-            //        let PAb = SAPtr(n - m);
-            //        let ISAb = SAPtr(m);
+            if (0 < m)
+            {
+                // Sort the type B* suffixes by their first two characters
+                let PAb = SAPtr(n - m);
+                let ISAb = SAPtr(m);
 
-            //        for i in (0..=(m - 2)).rev() {
-            //            t = SA[PAb + i];
-            //            c0 = T.get(t);
-            //            c1 = T.get(t + 1);
-            //            B.bstar()[(c0, c1)] -= 1;
-            //            SA[B.bstar()[(c0, c1)]] = i;
-            //        }
-            //        t = SA[PAb + m - 1];
-            //        c0 = T.get(t);
-            //        c1 = T.get(t + 1);
-            //        B.bstar()[(c0, c1)] -= 1;
-            //        SA[B.bstar()[(c0, c1)]] = m - 1;
+                for i in (0.. = (m - 2)).rev() {
+                    t = SA[PAb + i];
+                    c0 = T.get(t);
+                    c1 = T.get(t + 1);
+                    B.bstar()[(c0, c1)] -= 1;
+                    SA[B.bstar()[(c0, c1)]] = i;
+                }
+                t = SA[PAb + m - 1];
+                c0 = T.get(t);
+                c1 = T.get(t + 1);
+                B.bstar()[(c0, c1)] -= 1;
+                SA[B.bstar()[(c0, c1)]] = m - 1;
 
-            //        // Sort the type B* substrings using sssort.
-            //        let buf = SAPtr(m);
-            //        let bufsize = n - (2 * m);
+                // Sort the type B* substrings using sssort.
+                let buf = SAPtr(m);
+                let bufsize = n - (2 * m);
 
-            //        // init (outer)
-            //        c0 = ALPHABET_SIZE as Idx - 2;
-            //        j = m;
-            //        while 0 < j {
-            //            // init (inner)
-            //            c1 = ALPHABET_SIZE as Idx - 1;
-            //            while c0 < c1 {
-            //                // body (inner)
-            //                i = B.bstar()[(c0, c1)];
+                // init (outer)
+                c0 = ALPHABET_SIZE as Idx - 2;
+                j = m;
+                while 0 < j {
+                    // init (inner)
+                    c1 = ALPHABET_SIZE as Idx - 1;
+                    while c0 < c1 {
+                        // body (inner)
+                        i = B.bstar()[(c0, c1)];
 
-            //                if (1 < (j - i)) {
-            //                    SA_dump!(&SA.range(i..j), "sssort(A)");
-            //                    sssort::sssort(
-            //                        T,
-            //                        SA,
-            //                        PAb,
-            //                        SAPtr(i),
-            //                        SAPtr(j),
-            //                        buf,
-            //                        bufsize,
-            //                        2,
-            //                        n,
-            //                        SA[i] == (m - 1),
-            //                    );
-            //                    SA_dump!(&SA.range(i..j), "sssort(B)");
-            //                }
+                        if (1 < (j - i))
+                        {
+                            SA_dump!(&SA.range(i..j), "sssort(A)");
+                            sssort::sssort(
+                                T,
+                                SA,
+                                PAb,
+                                SAPtr(i),
+                                SAPtr(j),
+                                buf,
+                                bufsize,
+                                2,
+                                n,
+                                SA[i] == (m - 1),
 
-            //                // iter (inner)
-            //                j = i;
-            //                c1 -= 1;
-            //            }
+                            );
+                            SA_dump!(&SA.range(i..j), "sssort(B)");
+                        }
 
-            //            // iter (outer)
-            //            c0 -= 1;
-            //        }
+                        // iter (inner)
+                        j = i;
+                        c1 -= 1;
+                    }
 
-            //        // Compute ranks of type B* substrings
-            //        i = m - 1;
-            //        while 0 <= i {
-            //            if (0 <= SA[i]) {
-            //                j = i;
-            //                loop {
-            //                    {
-            //                        let SAi = SA[i];
-            //                        SA[ISAb + SAi] = i;
-            //                    }
+                    // iter (outer)
+                    c0 -= 1;
+                }
 
-            //                    i -= 1;
-            //                    if !((0 <= i) && (0 <= SA[i])) {
-            //                        break;
-            //                    }
-            //                }
+                // Compute ranks of type B* substrings
+                i = m - 1;
+                while 0 <= i {
+                    if (0 <= SA[i])
+                    {
+                        j = i;
+                        loop {
+                            {
+                                let SAi = SA[i];
+                                SA[ISAb + SAi] = i;
+                            }
 
-            //                SA[i + 1] = i - j;
-            //                if (i <= 0) {
-            //                    break;
-            //                }
-            //            }
-            //            j = i;
-            //            loop {
-            //                SA[i] = !SA[i];
-            //                {
-            //                    let idx = ISAb + SA[i];
-            //                    SA[idx] = j;
-            //                }
+                            i -= 1;
+                            if !((0 <= i) && (0 <= SA[i])) {
+                                break;
+                            }
+                        }
 
-            //                i -= 1;
-            //                if !(SA[i] < 0) {
-            //                    break;
-            //                }
-            //            }
-            //            {
-            //                let idx = ISAb + SA[i];
-            //                SA[idx] = j;
-            //            }
+                        SA[i + 1] = i - j;
+                        if (i <= 0)
+                        {
+                            break;
+                        }
+                    }
+                    j = i;
+                    loop {
+                        SA[i] = !SA[i];
+                        {
+                            let idx = ISAb + SA[i];
+                            SA[idx] = j;
+                        }
 
-            //            i -= 1;
-            //        }
+                        i -= 1;
+                        if !(SA[i] < 0) {
+                            break;
+                        }
+                    }
+                    {
+                        let idx = ISAb + SA[i];
+                        SA[idx] = j;
+                    }
 
-            //        // Construct the inverse suffix array of type B* suffixes using trsort.
-            //        trsort::trsort(ISAb, SA, m, 1);
+                    i -= 1;
+                }
 
-            //        // Set the sorted order of type B* suffixes
-            //        {
-            //            // init
-            //            i = n - 1;
-            //            j = m;
-            //            c0 = T.get(n - 1);
-            //            while 0 <= i {
-            //                // init
-            //                i -= 1;
-            //                c1 = c0;
+                //        // Construct the inverse suffix array of type B* suffixes using trsort.
+                //        trsort::trsort(ISAb, SA, m, 1);
 
-            //                loop {
-            //                    // cond
-            //                    if !(0 <= i) {
-            //                        break;
-            //                    }
-            //                    c0 = T.get(i);
-            //                    if !(c0 >= c1) {
-            //                        break;
-            //                    }
+                //        // Set the sorted order of type B* suffixes
+                //        {
+                //            // init
+                //            i = n - 1;
+                //            j = m;
+                //            c0 = T.get(n - 1);
+                //            while 0 <= i {
+                //                // init
+                //                i -= 1;
+                //                c1 = c0;
 
-            //                    // body (empty)
+                //                loop {
+                //                    // cond
+                //                    if !(0 <= i) {
+                //                        break;
+                //                    }
+                //                    c0 = T.get(i);
+                //                    if !(c0 >= c1) {
+                //                        break;
+                //                    }
 
-            //                    // iter
-            //                    i -= 1;
-            //                    c1 = c0;
-            //                }
+                //                    // body (empty)
 
-            //                if 0 <= i {
-            //                    t = i;
+                //                    // iter
+                //                    i -= 1;
+                //                    c1 = c0;
+                //                }
 
-            //                    // init
-            //                    i -= 1;
-            //                    c1 = c0;
+                //                if 0 <= i {
+                //                    t = i;
 
-            //                    loop {
-            //                        // cond
-            //                        if !(0 <= i) {
-            //                            break;
-            //                        }
-            //                        c0 = T.get(i);
-            //                        if !(c0 <= c1) {
-            //                            break;
-            //                        }
+                //                    // init
+                //                    i -= 1;
+                //                    c1 = c0;
 
-            //                        // body (empty)
+                //                    loop {
+                //                        // cond
+                //                        if !(0 <= i) {
+                //                            break;
+                //                        }
+                //                        c0 = T.get(i);
+                //                        if !(c0 <= c1) {
+                //                            break;
+                //                        }
 
-            //                        // iter
-            //                        i -= 1;
-            //                        c1 = c0;
-            //                    }
+                //                        // body (empty)
 
-            //                    j -= 1;
-            //                    {
-            //                        let pos = SA[ISAb + j];
-            //                        SA[pos] = if (t == 0) || (1 < (t - i)) { t } else { !t };
-            //                    }
-            //                }
-            //            }
-            //        } // End: Set the sorted order of type B* suffixes
+                //                        // iter
+                //                        i -= 1;
+                //                        c1 = c0;
+                //                    }
 
-            //        // Calculate the index of start/end point of each bucket
-            //        {
-            //            B.b()[(ALPHABET_SIZE as Idx - 1, ALPHABET_SIZE as Idx - 1)] = n; // end point
+                //                    j -= 1;
+                //                    {
+                //                        let pos = SA[ISAb + j];
+                //                        SA[pos] = if (t == 0) || (1 < (t - i)) { t } else { !t };
+                //                    }
+                //                }
+                //            }
+                //        } // End: Set the sorted order of type B* suffixes
 
-            //            // init
-            //            c0 = ALPHABET_SIZE as Idx - 2;
-            //            k = m - 1;
+                //        // Calculate the index of start/end point of each bucket
+                //        {
+                //            B.b()[(ALPHABET_SIZE as Idx - 1, ALPHABET_SIZE as Idx - 1)] = n; // end point
 
-            //            while 0 <= c0 {
-            //                i = A[c0 + 1] - 1;
+                //            // init
+                //            c0 = ALPHABET_SIZE as Idx - 2;
+                //            k = m - 1;
 
-            //                // init
-            //                c1 = ALPHABET_SIZE as Idx - 1;
-            //                while c0 < c1 {
-            //                    t = i - B.b()[(c0, c1)];
-            //                    B.b()[(c0, c1)] = i; // end point
+                //            while 0 <= c0 {
+                //                i = A[c0 + 1] - 1;
 
-            //                    // Move all type B* suffixes to the correct position
-            //                    {
-            //                        // init
-            //                        i = t;
-            //                        j = B.bstar()[(c0, c1)];
+                //                // init
+                //                c1 = ALPHABET_SIZE as Idx - 1;
+                //                while c0 < c1 {
+                //                    t = i - B.b()[(c0, c1)];
+                //                    B.b()[(c0, c1)] = i; // end point
 
-            //                        while j <= k {
-            //                            SA[i] = SA[k];
+                //                    // Move all type B* suffixes to the correct position
+                //                    {
+                //                        // init
+                //                        i = t;
+                //                        j = B.bstar()[(c0, c1)];
 
-            //                            // iter
-            //                            i -= 1;
-            //                            k -= 1;
-            //                        }
-            //                    } // End: Move all type B* suffixes to the correct position
+                //                        while j <= k {
+                //                            SA[i] = SA[k];
 
-            //                    // iter
-            //                    c1 -= 1;
-            //                }
-            //                B.bstar()[(c0, c0 + 1)] = i - B.b()[(c0, c0)] + 1;
-            //                B.b()[(c0, c0)] = i; // end point
+                //                            // iter
+                //                            i -= 1;
+                //                            k -= 1;
+                //                        }
+                //                    } // End: Move all type B* suffixes to the correct position
 
-            //                // iter
-            //                c0 -= 1;
-            //            }
-            //        } // End: Calculate the index of start/end point of each bucket
-            //    }
+                //                    // iter
+                //                    c1 -= 1;
+                //                }
+                //                B.bstar()[(c0, c0 + 1)] = i - B.b()[(c0, c0)] + 1;
+                //                B.b()[(c0, c0)] = i; // end point
 
-            //    SortTypeBstarResult { A, B, m }
+                //                // iter
+                //                c0 -= 1;
+                //            }
+                //        } // End: Calculate the index of start/end point of each bucket
+                //    }
+
+                //    SortTypeBstarResult { A, B, m }
+            }
+            //}
+
         }
-        //}
-
     }
-}
