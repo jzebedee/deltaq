ea5d669f74eaff377a7b21791a09afca3a80d5dc
J. Zebedee <zebedee@code.gripe>
2021-10-28 03:59:13 -0500

    Add LibDivSufSort test stub

---
+++ test/DeltaQ.SuffixSorting.LivDivSufSort.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DeltaQ.SuffixSorting.LivDivSufSort.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
