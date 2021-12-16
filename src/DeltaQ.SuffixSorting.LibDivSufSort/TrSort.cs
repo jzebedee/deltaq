ac3fe946d613f64589fe03217d754e374b5639d3
J. Zebedee <zebedee@code.gripe>
2021-12-15 20:49:38 -0500

    Break out Budget

--- src/DeltaQ.SuffixSorting.LibDivSufSort/TrSort.cs
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/TrSort.cs
@@ -38,41 +38,6 @@ internal static class TrSort
         }
     }
 
-    private ref struct Budget
-    {
-        public int Chance;
-        public int Remain;
-        public int IncVal;
-        public int Count;
-
-        public Budget(int chance, int incVal)
-        {
-            Chance = chance;
-            Remain = incVal;
-            IncVal = incVal;
-            Count = 0;
-        }
-
-        public bool Check(int size)
-        {
-            if (size <= Remain)
-            {
-                Remain -= size;
-                return true;
-            }
-
-            if (Chance == 0)
-            {
-                Count += size;
-                return false;
-            }
-
-            Remain += IncVal - size;
-            Chance -= 1;
-            return true;
-        }
-    }
-
     /// Tandem repeat sort
     internal static void trsort(SAPtr ISA, Span<int> SA, int n, int depth)
     {
