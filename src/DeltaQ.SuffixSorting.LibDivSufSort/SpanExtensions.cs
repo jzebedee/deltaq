f7dc13485b04156fcf07e942bffa8e4eba14a4ad
J. Zebedee <zebedee@code.gripe>
2021-12-17 03:52:26 -0500

    Flatten namespace in SpanExtensions

--- src/DeltaQ.SuffixSorting.LibDivSufSort/SpanExtensions.cs
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/SpanExtensions.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace DeltaQ.SuffixSorting.LibDivSufSort
+namespace DeltaQ.SuffixSorting.LibDivSufSort;
+
+internal static class SpanExtensions
 {
-    internal static class SpanExtensions
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Swap<T>(this Span<T> span, int i, int j)
-            => (span[j], span[i]) = (span[i], span[j]);
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Swap<T>(this Span<T> span, int i, int j)
+        => (span[j], span[i]) = (span[i], span[j]);
 }
