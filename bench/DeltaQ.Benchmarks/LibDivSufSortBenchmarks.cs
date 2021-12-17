f93e3812fafbe0f98ee07e0b02de648648d66bc1
J. Zebedee <zebedee@code.gripe>
2021-12-16 20:44:10 -0500

    Add LibDivSufSort benchmark

---
+++ bench/DeltaQ.Benchmarks/LibDivSufSortBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using DeltaQ.SuffixSorting.LibDivSufSort;
+
+namespace DeltaQ.Benchmarks
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    public class LibDivSufSortBenchmarks
+    {
+        private LibDivSufSort ldss = new LibDivSufSort();
+
+        private static readonly byte[][] _assets = Directory.EnumerateFiles("./assets/").Select(File.ReadAllBytes).ToArray();
+
+        [Benchmark(Baseline = true)]
+        public void ss_compare_old()
+        {
+            SsSort.new_ss_compare_feature_flag = false;
+            foreach (var asset in _assets)
+            {
+                ldss.Sort(asset).Dispose();
+            }
+        }
+
+        [Benchmark]
+        public void ss_compare_new()
+        {
+            SsSort.new_ss_compare_feature_flag = true;
+            foreach (var asset in _assets)
+            {
+                ldss.Sort(asset).Dispose();
+            }
+        }
+    }
+}
