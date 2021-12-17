1c25c6d78985f2ff89279210e7cdbe721ad32d52
J. Zebedee <zebedee@code.gripe>
2021-12-17 03:52:53 -0500

    Add inlining and remove IntAccessor in TdPAStarAccessor

--- src/DeltaQ.SuffixSorting.LibDivSufSort/TdPAStarAccessor.cs
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/TdPAStarAccessor.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using Text = System.ReadOnlySpan<byte>;
 
 namespace DeltaQ.SuffixSorting.LibDivSufSort;
 
@@ -7,18 +9,21 @@ internal ref struct TdPAStarAccessor
     private readonly ReadOnlySpanOffsetAccessor<byte> _TO;
     private readonly ReadOnlySpan<int> _SA;
     private readonly ReadOnlySpan<int> _PA;
-    private readonly IntAccessor _TD;
 
-    public TdPAStarAccessor(ReadOnlySpan<byte> T, ReadOnlySpan<int> SA, int partitionOffset, int tdOffset)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public TdPAStarAccessor(Text T, ReadOnlySpan<int> SA, int partitionOffset, int tdOffset)
     {
         _TO = new ReadOnlySpanOffsetAccessor<byte>(T, tdOffset);
-
         _SA = SA;
         _PA = SA[partitionOffset..];
-        _TD = new(T[tdOffset..]);
     }
 
-    public readonly int this[int index] => _TD[_PA[_SA[index]]];
+    public readonly int this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _TO[_PA[_SA[index]]];
+    }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly int AsOffset(int index) => _TO[index];
 }
