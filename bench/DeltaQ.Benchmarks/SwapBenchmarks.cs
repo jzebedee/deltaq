24538255fd06209969b2a8faf4def4dea168931c
J. Zebedee <zebedee@code.gripe>
2021-12-18 10:41:06 -0500

    add SwapBenchmarks

---
+++ bench/DeltaQ.Benchmarks/SwapBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Runtime.CompilerServices;
+
+namespace DeltaQ.Benchmarks
+{
+    [SimpleJob]
+    public class SwapBenchmarks
+    {
+        [Benchmark]
+        public void SwapTupleByte()
+        {
+            Span<byte> bytes = stackalloc byte[] { 0, 10, 15, 9, 12 };
+            Swap(bytes, 0, 4);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SwapTempByte()
+        {
+            Span<byte> bytes = stackalloc byte[] { 0, 10, 15, 9, 12 };
+            Swap(ref bytes[0], ref bytes[4]);
+        }
+
+        [Benchmark]
+        public void SwapTupleInt()
+        {
+            Span<int> ints = stackalloc int[] { 0, 10, 15, 9, 12 };
+            Swap(ints, 0, 4);
+        }
+
+        [Benchmark]
+        public void SwapTempInt()
+        {
+            Span<int> ints = stackalloc int[] { 0, 10, 15, 9, 12 };
+            Swap(ref ints[0], ref ints[4]);
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Swap<T>(Span<T> span, int i, int j)
+            => (span[j], span[i]) = (span[i], span[j]);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
