9b05eba06d58248f2bc78cf7578ae7d9985f51d7
J. Zebedee <zebedee@code.gripe>
2021-12-19 10:02:56 -0500

    Update benchmarks and LDSS benchmarks

--- bench/DeltaQ.Benchmarks/LibDivSufSortBenchmarks.cs
+++ bench/DeltaQ.Benchmarks/LibDivSufSortBenchmarks.cs
@@ -7,9 +7,30 @@ namespace DeltaQ.Benchmarks
     [SimpleJob(RunStrategy.Throughput)]
     public class LibDivSufSortBenchmarks
     {
-        private LibDivSufSort ldss = new LibDivSufSort();
-
         private static readonly byte[][] _assets = Directory.EnumerateFiles("./assets/").Select(File.ReadAllBytes).ToArray();
 
+        [Benchmark(Baseline = true)]
+        public void ldss()
+        {
+            //SsSort.new_ss_pivot_feature_flag = false;
+
+            var ldss = new LibDivSufSort();
+            foreach (var asset in _assets)
+            {
+                ldss.Sort(asset).Dispose();
+            }
+        }
+
+        //[Benchmark]
+        //public void ss_pivot_new()
+        //{
+        //    SsSort.new_ss_pivot_feature_flag = true;
+
+        //    var ldss = new LibDivSufSort();
+        //    foreach (var asset in _assets)
+        //    {
+        //        ldss.Sort(asset).Dispose();
+        //    }
+        //}
     }
 }
