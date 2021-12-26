7b10fa916cc1a6ddc8914921f2f4c89299b4f391
J. Zebedee <zebedee@code.gripe>
2021-12-26 07:07:28 -0500

    Add netstandard2.0 fallbacks to DeltaQ.Utility.Memory

---
+++ src/DeltaQ.Utility.Memory/HashHelpers.cs
@@ -0,0 +1,21 @@
+﻿//Generated 2021-12-26
+//https://raw.githubusercontent.com/dotnet/runtime/84680bf557210114ea5ca823386cd49691c4cac6/src/libraries/System.Private.CoreLib/src/System/Numerics/Hashing/HashHelpers.cs
+
+#if NETSTANDARD2_0
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Numerics.Hashing
+{
+    internal static class HashHelpers
+    {
+        public static int Combine(int h1, int h2)
+        {
+            // RyuJIT optimizes this to use the ROL instruction
+            // Related GitHub pull request: https://github.com/dotnet/coreclr/pull/1830
+            uint rol5 = ((uint)h1 << 5) | ((uint)h1 >> 27);
+            return ((int)rol5 + h1) ^ h2;
+        }
+    }
+}
+#endif
