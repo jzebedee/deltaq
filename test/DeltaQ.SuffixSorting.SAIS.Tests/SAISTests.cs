163f195a1373262f2e1f4bfc6cd3716ee571174f
J. Zebedee <zebedee@code.gripe>
2021-12-28 08:04:40 -0500

    Remove net461 directives in SAISTests

--- test/DeltaQ.SuffixSorting.SAIS.Tests/SAISTests.cs
+++ test/DeltaQ.SuffixSorting.SAIS.Tests/SAISTests.cs
@@ -11,13 +11,6 @@ namespace DeltaQ.Tests
     {
         private readonly SAIS _sais = new SAIS();
 
-#if NET461
-        private static void RandomFillBuffer(byte[] buffer)
-        {
-            var rand = new Random(63 * 13 * 63 * 13);
-            rand.NextBytes(buffer);
-        }
-#else
         private static SpanOwner<byte> GetOwnedRandomBuffer(int size)
         {
             var rand = new Random(63 * 13 * 63 * 13);
@@ -33,7 +26,6 @@ namespace DeltaQ.Tests
 
             return owner;
         }
-#endif
 
         [Theory]
         [InlineData(0)]
@@ -44,61 +36,18 @@ namespace DeltaQ.Tests
         [InlineData(16)]
         [InlineData(32)]
         [InlineData(51)]
+        [InlineData(0x8000 - 1)]
         [InlineData(0x8000)]
-        [InlineData(0x80000)]
-        [InlineData(0x800000)]
         public void CheckRandomBuffer(int size)
         {
-#if NET461
-            var ownedT = ArrayPool<byte>.Shared.Rent(size);
-            try
-#else
-            using (var ownedT = GetOwnedRandomBuffer(size))
-#endif
-            {
-#if NET461
-                RandomFillBuffer(ownedT);
-                Span<byte> T = ownedT;
-#else
-                Span<byte> T = ownedT.Span;
-#endif
-                using (var ownedSA = _sais.Sort(T))
-                {
-                    Span<int> SA = ownedSA.Span;
-                    var result = Check(T, SA, T.Length, false);
-                    Assert.Equal(0, result);
-                }
-            }
-#if NET461
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(ownedT);
-            }
-#endif
-        }
+            using var ownedT = GetOwnedRandomBuffer(size);
+            Span<byte> T = ownedT.Span;
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(4)]
-        [InlineData(8)]
-        [InlineData(16)]
-        [InlineData(32)]
-        [InlineData(51)]
-        [InlineData(0x1000)]
-        public void CheckRandomBufferContinuous(int size)
-        {
-            const int repetitions = 2_000;
-            for (int i = 0; i < repetitions; i++)
-            {
-                CheckRandomBuffer(size);
+            using var ownedSA = _sais.Sort(T);
+            Span<int> SA = ownedSA.Span;
 
-                if (i % 100 == 0)
-                {
-                    System.Diagnostics.Debug.WriteLine("Gen0:{0} Gen1:{1} Gen2:{2}", GC.CollectionCount(0), GC.CollectionCount(1), GC.CollectionCount(2));
-                }
-            }
+            var result = Check(T, SA, T.Length, false);
+            Assert.Equal(0, result);
         }
     }
 }
