41ca482769610acabbd9fc1d5ba39b206b1d6107
J. Zebedee <zebedee@code.gripe>
2021-12-29 05:50:08 -0500

    Add ApplyCommand

---
+++ src/DeltaQ.CommandLine/Commands.Apply.cs
@@ -0,0 +1,54 @@
+﻿using Humanizer;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace DeltaQ.CommandLine;
+using static Defaults;
+
+internal static partial class Commands
+{
+    public static Action<CommandLineApplication> ApplyCommand { get; } = command =>
+    {
+        command.Description = "Apply a delta (patch) to an original file and generate an output file";
+        command.HelpOption(HelpOptions);
+
+        var oldFileArg = command.Argument("[oldfile]", "Original file (input)");
+        var deltaFileArg = command.Argument("[deltafile]", "Delta file (input)");
+        var newFileArg = command.Argument("[newfile]", "New file (output)");
+
+        command.OnExecute(() =>
+        {
+            var oldFile = oldFileArg.Value;
+            var newFile = newFileArg.Value;
+            var deltaFile = deltaFileArg.Value;
+            Console.WriteLine("Applying BsDiff delta between");
+            Console.WriteLine($@"Old file:   ""{oldFile}""");
+            Console.WriteLine($@"Delta file: ""{deltaFile}""");
+            Console.WriteLine();
+            try
+            {
+                var sw = System.Diagnostics.Stopwatch.StartNew();
+                {
+                    using var fsInput = File.OpenRead(oldFile);
+                    using var fsDelta = MemoryMappedFile.CreateFromFile(deltaFile, FileMode.Open, null, 0, MemoryMappedFileAccess.Read);
+                    using var fsOutput = File.Create(newFile);
+                    BsDiff.Patch.Apply(fsInput, OpenPatch, fsOutput);
+
+                    Stream OpenPatch(long offset, long length) => fsDelta.CreateViewStream(offset, length, MemoryMappedFileAccess.Read);
+                }
+                sw.Stop();
+
+                Console.WriteLine($"Finished in {sw.Elapsed.Humanize()} [{sw.Elapsed}]");
+                Console.WriteLine($@"New file: ""{newFile}""");
+            }
+            catch
+            {
+                Console.Error.WriteLine("Failed to apply delta");
+                throw;
+            }
+            return 0;
+        });
+    };
+}
