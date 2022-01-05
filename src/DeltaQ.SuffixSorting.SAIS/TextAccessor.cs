c29655dc0fdf88481102c1a16221517cdd12bc03
J. Zebedee <zebedee@code.gripe>
2022-01-04 22:59:56 -0500

    Add TA stub

---
+++ src/DeltaQ.SuffixSorting.SAIS/TextAccessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeltaQ.SuffixSorting.SAIS
+{
+    internal ref struct TextAccessor<T>
+    {
+        private readonly ReadOnlySpan<T> _text;
+        public TextAccessor(ReadOnlySpan<T> text)
+        {
+            _text = text;
+        }
+
+        public ref readonly T this[int index] => ref _text[index];
+    }
+}
