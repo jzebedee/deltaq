1d445dc760191a0a5039c14c11a8d63e4d7748c5
J. Zebedee <zebedee@code.gripe>
2022-01-08 15:01:59 -0500

    Use smaller test set and remove dependencies in Log2Benchmarks

--- bench/DeltaQ.Benchmarks/Log2Benchmarks.cs
+++ bench/DeltaQ.Benchmarks/Log2Benchmarks.cs
@@ -1,115 +1,117 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System.Runtime.CompilerServices;
 using Idx = System.Int32;
-using static DeltaQ.SuffixSorting.LibDivSufSort.Utils;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Engines;
+using System.Numerics;
 
 namespace DeltaQ.Benchmarks
 {
-    //[HardwareCounters(HardwareCounter.BranchInstructions, HardwareCounter.BranchMispredictions)]
+    [HardwareCounters(HardwareCounter.BranchInstructions, HardwareCounter.BranchMispredictions)]
     [SimpleJob(RunStrategy.Throughput)]
     public class Log2Benchmarks
     {
-        private const int Step = 1;
+        public static IEnumerable<int> Numbers { get; } = new[] { 0, 1, 2, 32, 51, 1024, 2000, 48000, int.MaxValue };
 
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark(Baseline = true)]
-        public void tr_ilg()
+        public void tr_ilg(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = tr_ilg(i);
-            }
+            Idx y = tr_ilg_core(i);
             GC.KeepAlive(y);
         }
 
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark]
-        public void Log2()
+        public void Log2(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = Log2(i);
-            }
+            Idx y = Log2_core(i);
+            GC.KeepAlive(y);
+        }
+
+        [ArgumentsSource(nameof(Numbers))]
+        [Benchmark]
+        public void Log2BitOps(int i)
+        {
+            Idx y = Log2BitOps_core((uint)i);
             GC.KeepAlive(y);
         }
 
 #if NETCOREAPP3_0_OR_GREATER
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark]
-        public void MathLog2()
+        public void MathLog2(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = (int)Math.Log2(i);
-            }
+            Idx y = (int)Math.Log2(i);
             GC.KeepAlive(y);
         }
 
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark]
-        public void MathFLog2()
+        public void MathFLog2(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = (int)MathF.Log2(i);
-            }
+            Idx y = (int)MathF.Log2(i);
             GC.KeepAlive(y);
         }
 
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark]
-        public void MathILogB()
+        public void MathILogB(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = Math.ILogB(i);
-            }
+            Idx y = Math.ILogB(i);
             GC.KeepAlive(y);
         }
 
+        [ArgumentsSource(nameof(Numbers))]
         [Benchmark]
-        public void MathFILogB()
+        public void MathFILogB(int i)
         {
-            Idx y = -1;
-            for (int i = 0; i < int.MaxValue; i += Step)
-            {
-                y = MathF.ILogB(i);
-            }
+            Idx y = MathF.ILogB(i);
             GC.KeepAlive(y);
         }
 #endif
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int tr_ilg(int n)
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        private static int tr_ilg_core(int n)
         {
             if ((n & 0xffff_0000) > 0)
             {
                 if ((n & 0xff00_0000) > 0)
                 {
-                    return 24 + lg_table[((n >> 24) & 0xff)];
+                    return 24 + lg_table[(n >> 24) & 0xff];
                 }
                 else
                 {
-                    return 16 + lg_table[((n >> 16) & 0xff)];
+                    return 16 + lg_table[(n >> 16) & 0xff];
                 }
             }
             else
             {
                 if ((n & 0x0000_ff00) > 0)
                 {
-                    return 8 + lg_table[((n >> 8) & 0xff)];
+                    return 8 + lg_table[(n >> 8) & 0xff];
                 }
                 else
                 {
-                    return 0 + lg_table[((n >> 0) & 0xff)];
+                    return 0 + lg_table[(n >> 0) & 0xff];
                 }
             }
         }
+        private static readonly int[] lg_table_array = new[]
+        {
+         -1,0,1,1,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
+          5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
+          6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+          6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
+          7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+          7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+          7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+          7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+        };
+        private static ReadOnlySpan<int> lg_table => lg_table_array;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Log2(int v)
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public static int Log2_core(int v)
         {
             int r = 0xFFFF - v >> 31 & 0x10;
             v >>= r;
@@ -122,8 +124,11 @@ namespace DeltaQ.Benchmarks
             shift = 0x3 - v >> 31 & 0x2;
             v >>= shift;
             r |= shift;
-            r |= (v >> 1);
+            r |= v >> 1;
             return r;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public static int Log2BitOps_core(uint v) => BitOperations.Log2(v);
     }
 }
