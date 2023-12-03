e065bdfcc0a1ec34088fc51905869b728d9f5613
J. Zebedee <zebedee@code.gripe>
2023-12-03 15:55:02 -0600

    Fold ROSOffsetAccessor into TdPAStarAccessor and use primary constructor with ref readonly

--- src/DeltaQ.SuffixSorting.LibDivSufSort/TdPAStarAccessor.cs
+++ src/DeltaQ.SuffixSorting.LibDivSufSort/TdPAStarAccessor.cs
@@ -4,26 +4,19 @@ using Text = System.ReadOnlySpan<byte>;
 
 namespace DeltaQ.SuffixSorting.LibDivSufSort;
 
-internal ref struct TdPAStarAccessor
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+internal readonly ref struct TdPAStarAccessor(Text T, ReadOnlySpan<int> SA, int partitionOffset, int tdOffset)
 {
-    private readonly ReadOnlySpanOffsetAccessor<byte> _TO;
-    private readonly ReadOnlySpan<int> _SA;
-    private readonly ReadOnlySpan<int> _PA;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public TdPAStarAccessor(Text T, ReadOnlySpan<int> SA, int partitionOffset, int tdOffset)
-    {
-        _TO = new ReadOnlySpanOffsetAccessor<byte>(T, tdOffset);
-        _SA = SA;
-        _PA = SA[partitionOffset..];
-    }
+    private readonly ReadOnlySpan<byte> _TO = T;
+    private readonly ReadOnlySpan<int> _SA = SA;
+    private readonly ReadOnlySpan<int> _PA = SA[partitionOffset..];
 
     public readonly int this[int index]
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => _TO[_PA[_SA[index]]];
+        get => _TO[_PA[_SA[index]] + tdOffset];
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly int AsOffset(int index) => _TO[index];
+    public readonly int AsOffset(int index) => _TO[index + tdOffset];
 }
