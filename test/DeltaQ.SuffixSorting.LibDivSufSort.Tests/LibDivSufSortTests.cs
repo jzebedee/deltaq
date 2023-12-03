eb28b6befc811ce7c626c4ec33776d910e6585a5
J. Zebedee <zebedee@code.gripe>
2023-12-03 13:48:56 -0600

    Retarget Microsoft.Toolkit.HighPerformance to CommunityToolkit.HighPerformance

--- test/DeltaQ.SuffixSorting.LibDivSufSort.Tests/LibDivSufSortTests.cs
+++ test/DeltaQ.SuffixSorting.LibDivSufSort.Tests/LibDivSufSortTests.cs
@@ -1,5 +1,5 @@
+using CommunityToolkit.HighPerformance.Buffers;
 using DeltaQ.SuffixSorting.LibDivSufSort;
-using Microsoft.Toolkit.HighPerformance.Buffers;
 using System;
 using System.Collections.Generic;
 using System.IO;
